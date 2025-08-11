ca260979193eb1a537d343ceac1425dc9932d252
losrobbos <robert.ristock@googlemail.com>
2025-08-11 12:32:26 +0200

    Initial commit

---
+++ src/app/blog/[id]/page.tsx
@@ -0,0 +1,12 @@
+export default async function BlogDetails(params: { params: Promise<{ id: string }> }) {
+
+  const { id } = await params.params;
+  console.log("Blog ID:", id);
+
+  return (
+    <section>
+      <h1>Blog Details {id}</h1>
+      <p>This is the blog details page.</p>
+    </section>
+  );
+}
---
+++ src/app/blog/page.tsx
@@ -0,0 +1,8 @@
+export default async function BlogPage() {
+  return (
+    <section>
+      <h1>Blog</h1>
+      <p>This is the blog page.</p>
+    </section>
+  );
+} 
