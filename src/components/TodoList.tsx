87616e0e943a03bcf9173bb53e1d453d96f8aa0f
losrobbos <losrobbos@web.de>
2025-08-13 12:45:08 +0200

    Added comments

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,5 +1,6 @@
 export default async function TodoList() {
 
+  // simuliere eine lange Datenabfrage (=> diese Component braucht etwas, um nachgeladen zu werden)
   const result: string = await new Promise(resolve => setTimeout(() => resolve("GOLD NUGGET"), 3000)); // Simulate data fetching delay
 
   return (
